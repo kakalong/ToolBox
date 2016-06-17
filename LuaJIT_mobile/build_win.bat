5e12f13e1a0f4fc41d6c3773301ae1e0160e75f0
kakalong <kakalong913@gmail.com>
2016-06-17 18:45:55 +0800

    update win compile

--- LuaJIT_mobile/build_win.bat
+++ LuaJIT_mobile/build_win.bat
@@ -15,13 +15,15 @@ echo Swtich to x86 build env
 cd luajit\src
 call msvcbuild.bat
 copy /Y lua51.dll ..\..\prebuilt\win\x86\slua.dll
+copy /Y luajit.exe ..\..\prebuilt\win\x86\luajit.exe
 cd ..\..
 
 call "%ENV64%"
 echo Swtich to x64 build env
 cd luajit\src
-call msvcbuild.bat
+call msvcbuild64.bat
 copy /Y lua51.dll ..\..\prebuilt\win\x64\slua.dll
+copy /Y luajit.exe ..\..\prebuilt\win\x64\luajit.exe
 cd ..\..
 
 
